e01e182ada169e7ee4acacb46ea9a337e12f3af5
SEHao <hchao@live.nl>
2025-02-16 14:51:33 +0100

    chore: apply prime theming

--- apps/client/src/app/app.config.ts
+++ apps/client/src/app/app.config.ts
@@ -1,9 +1,23 @@
+import { providePrimeNG } from 'primeng/config';
+
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
+import Aura from '@primeng/themes/aura';
+
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: false
+        }
+      },
+    }),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
   ],
